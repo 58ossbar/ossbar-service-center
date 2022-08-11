aa5936475afecae0ad2403ab741d9c21887f7de2
zhujianwu <zhujianwu@creatorblue.com>
2022-08-11 17:20:59 +0800

    服务端启动入口程序

---
+++ ossbar/src/main/java/com/ossbar/CreatorbluePlatformApplication.java
@@ -0,0 +1,16 @@
+package com.ossbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 服务端入口程序
+ */
+@SpringBootApplication
+public class CreatorbluePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatorbluePlatformApplication.class, args);
+	}
+
+}
