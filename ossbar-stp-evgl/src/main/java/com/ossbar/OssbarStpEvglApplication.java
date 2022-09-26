6a488e9f1953a5067b697576b2c42eb55be82f89
hujun <hujun@creatorblue.com>
2022-09-26 09:05:58 +0800

    更新

--- ossbar-stp-evgl/src/main/java/com/ossbar/OssbarStpEvglApplication.java
+++ ossbar-stp-evgl/src/main/java/com/ossbar/OssbarStpEvglApplication.java
@@ -2,8 +2,10 @@ package com.ossbar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class OssbarStpEvglApplication {
 
     public static void main(String[] args) {
